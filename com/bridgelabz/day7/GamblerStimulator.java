c1dd9f40ad3a055dc6a10db7432f15810c680ece
Sachin Lagad <sachinlagad56@gmail.com>
2022-11-25 15:39:21 +0530

    Initialcommit

---
+++ com/bridgelabz/day7/GamblerStimulator.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.day7;
+
+import java.util.Random;
+
+public class GamblerStimulator {
+	
+	int stake = 100;
+
+	public void putBet() {
+		Random rnd = new Random();
+		int reslt = rnd.nextInt(2);
+		if (reslt == 0) {
+			System.out.println("Lost bet of 1$!");
+		} else {
+			System.out.println("Won bet of 1$!");
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		GamblerStimulator gm = new GamblerStimulator();
+		gm.putBet();
+	}
+
+}
